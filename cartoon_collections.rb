118018de4e78673f67b5c66231bdc87be6edd722
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-20 16:30:46 +0300

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+require "pry"
+
+def roll_call_dwarves(dwarves_names)
   # Your code here
+  dwarves_names.each.with_index(1) { |dwarf_name, index|
+    puts "#{index}. #{dwarf_name}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
   # Your code here
+  planeteer_calls.map do |planeteer_call|
+    planeteer_call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |planeteer_call| planeteer_call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.find do |food| cheese_types.include?(food) end
 end
+
+binding.pry
